a6f39e20d047b5d16c3b270e465598fdd7213449
RamithR <ramithrandeniya@gmail.com>
2020-06-11 11:03:44 +0530

    code refactoring and added comments

--- functions/index.js
+++ functions/index.js
@@ -4,25 +4,43 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendNotification = functions.firestore.document('auction_items/{uid}').onUpdate(async (event) => {
 
+    //retrieve the UID of the highest bidder
      const userId = event.after.get('latest_bid_uid');
+
+     //retrieve the auction item title for notification header
      const title = event.after.get('auction_title');
+
+     //body of the notification
      const content = "You have been outbid, bid again to get on top!";
+
+     //retrieve the bidding history array for the item
      let bidHistory = event.after.get('bidding_history');
 
      let previousBidderArr = [];
 
+     //loop through the bidding history array and get all UIDs of auction participants
      for (var i = 0; i < bidHistory.length; i++) {
 
-        if(bidHistory[i].user_id !== userId){
-            previousBidderArr[i] = bidHistory[i].user_id;
-        }
+        previousBidderArr[i] = bidHistory[i].user_id;
     
      }
 
-     var biddersArr = filterRepeatedUids(previousBidderArr);
+     //eliminate repeated UIDs from array
+     var biddersArr =  previousBidderArr.filter( function( item, index, inputArray ) {
+        return inputArray.indexOf(item) === index;
+    });
 
+     console.log(biddersArr);
+
+     //remove the UID of the highest bidder because we only want to send the notification to users who have been outbid
+    var index = biddersArr.indexOf(userId); 
+    if (index > -1) { //if found
+        biddersArr.splice(index, 1);
+    }
 
      let fcmTokensArr = [];
+
+     //Read the registered_users document using the UIDs and retrieving their FCM tokens into an array
 
      /* eslint-disable no-await-in-loop */
      for(var k = 0; k < biddersArr.length; k++){
@@ -35,6 +53,9 @@ exports.sendNotification = functions.firestore.document('auction_items/{uid}').o
      /* eslint-disable no-await-in-loop */
 
      console.log(fcmTokensArr);
+     
+
+     //For each FCM token in array, fire the notification
 
      /* eslint-disable no-await-in-loop */
      for(var m = 0; m < fcmTokensArr.length; m++){
@@ -56,13 +77,3 @@ exports.sendNotification = functions.firestore.document('auction_items/{uid}').o
 
 });
 
-function filterRepeatedUids(array){
-    var len = array.length;
-    for(var i = 0; i < len; i++) for(var j = i + 1; j < len; j++) 
-        if(array[j] === array[i]){
-            array.splice(j,1);
-            j--;
-            len--;
-        }
-    return array;
-}
